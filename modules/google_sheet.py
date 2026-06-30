091fd5292dcbce3c28fd1bb1008a636f9e4a0a64
prathmeshmasurkar8 <103698143+prathmeshmasurkar8@users.noreply.github.com>
2026-06-30 20:47:05 +0530

    Implement Google Sheets integration
    Added functions to get and write data to Google Sheets.

--- modules/google_sheet.py
+++ modules/google_sheet.py
@@ -1,1 +1,37 @@
-print('Google Sheet Module')
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+def get_sheet():
+    creds = Credentials.from_service_account_file(
+        "credentials.json",
+        scopes=SCOPES
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open("NSE AI").worksheet("Recommendation")
+
+    return sheet
+
+
+def write_test():
+    sheet = get_sheet()
+
+    sheet.clear()
+
+    data = [
+        ["Rank", "Stock", "Score"],
+        [1, "RELIANCE", 95],
+        [2, "TCS", 92],
+        [3, "BEL", 90]
+    ]
+
+    sheet.update("A1", data)
+
+    print("Google Sheet Updated Successfully")
